16cfdf61e70077a0e38a8e4be5e113885fd7847e
Jakub Dylus <SuiMachine@users.noreply.github.com>
2022-02-26 11:18:41 +0100

    eeeeee

--- Doom64Hack/CameraHack.cpp
+++ Doom64Hack/CameraHack.cpp
@@ -38,100 +38,6 @@ float convertValueForNormalization(float valueToConvert)
 	return _mm_cvtss_f32(whateverThatIs);
 }
 
-char __fastcall handleInput1(__int64 a1, __int64 a2, unsigned __int64 a3)
-{
-	float v4; // xmm4_4
-	float v6; // xmm4_4
-	float v7; // xmm5_4
-	signed __int64 v8; // rax
-	__int64 v9; // rdx
-	unsigned __int64 v10; // r8
-	int v11; // edx
-	__int64 v12; // rdx
-	unsigned __int64 v13; // r8
-
-	if (!(*(BYTE*)(a1 + 0x224) & 4))
-	{
-		*(DWORD*)(*(ULONG*)a1 + 0x68i64) += 2 * *(DWORD*)(a1 + 0x14);
-		*(DWORD*)(*(ULONG*)a1 + 0x6Ci64) += 2 * *(DWORD*)(a1 + 0x18);
-
-
-		float inputXAxis = *(float*)(a1 + 0x270);
-		if (inputXAxis != 0.0)
-		{
-			int normalizedRotationX = normalizeInput(convertValueForNormalization(inputXAxis));
-			if (inputXAxis < 0.0f)
-				*(DWORD*)(*(ULONG*)a1 + 0x68i64) += normalizedRotationX;// Turn left
-			else
-				*(DWORD*)(*(ULONG*)a1 + 0x68i64) -= normalizedRotationX;// Turn right
-		}
-
-		float inputYAxis = *(float*)(a1 + 0x274);
-		if (inputYAxis != 0.0)
-		{
-			int normalizedRotationY = normalizeInput(convertValueForNormalization(inputYAxis));
-
-			auto currentOrientationY = *(int*)(*(ULONG*)a1 + 0x6Ci64);
-			if (inputYAxis < 0.0f)
-			{
-				auto newYOrientation = currentOrientationY + normalizedRotationY;
-				if (1104062110 < (currentOrientationY + normalizedRotationY))
-					*(int*)(*(ULONG*)a1 + 0x6Ci64) = 1104062110;
-				else
-					*(int*)(*(ULONG*)a1 + 0x6Ci64) = newYOrientation;
-			}
-			else
-			{
-				auto newYOrientation = currentOrientationY - normalizedRotationY;
-				if (-1071386491 > newYOrientation)
-					*(int*)(*(ULONG*)a1 + 0x6Ci64) = -1071386491;
-				else
-					*(int*)(*(ULONG*)a1 + 0x6Ci64) = newYOrientation;
-			}
-		}
-
-		/*
-		wchar_t text[14] = { 0 };
-		swprintf(text, 14, L"%d, %d\n", inputXAxis, inputYAxis);
-		OutputDebugStringW(text);	*/	
-	}
-	v8 = *(DWORD*)(a1 + 12);
-
-	int* someArray = (int*)(0x1405ED8A0);
-	if ((DWORD)v8 && *(BYTE*)(a1 + 560))
-	{
-		v9 = 2 * (signed int)v8;
-		v10 = (unsigned __int64)*(unsigned int*)(*(ULONG*)a1 + 0x68i64) >> 19;
-		*(DWORD*)(*(ULONG*)a1 + 0x88i64) += v9 * someArray[v10 + 2048] >> 16;
-		v8 = v9 * someArray[v10] >> 16;
-		*(DWORD*)(*(ULONG*)a1 + 140i64) += v8;
-	}
-	v11 = *(DWORD*)(a1 + 16);
-	if (v11 && *(BYTE*)(a1 + 560))
-	{
-		v12 = 2 * v11;
-		v13 = (unsigned __int64)(unsigned int)(*(DWORD*)(*(ULONG*)a1 + 0x68i64) - 0x40000000) >> 19;
-		*(DWORD*)(*(ULONG*)a1 + 0x88i64) += v12 * someArray[v13 + 2048] >> 16;
-		v8 = v12 * someArray[v13] >> 16;
-		*(DWORD*)(*(ULONG*)a1 + 0x8Ci64) += v8;
-	}
-	if (*(DWORD*)(a1 + 12) || *(DWORD*)(a1 + 16))
-	{
-		v8 = 0x1403D8B30;
-
-		if (*(signed long long**)(*(ULONG*)a1 + 0xA8i64) == &v8)
-		{
-			auto result = unknownHandleInputFunction2(*(ULONG*)a1, 2);
-			v8 &= ~255;
-			v8 |= result;
-		}
-	}
-	*(DWORD*)(a1 + 28) &= 0xFFFFFEFF;
-	*(DWORD*)(a1 + 160) = 0;
-	return v8;
-}
-
-
 void handleInput2()
 {
 
@@ -216,7 +122,5 @@ CameraHack::CameraHack()
 	unknownHandleInputFunction = (unknownHandleInputFunctionT)0x140177930;
 	unknownHandleInputFunction2 = (unknownHandleInputFunctionF1T)0x14015C130;
 
-
-	HookJmpTrampoline(0x140176E20, handleInput1, 0xE4);
 	HookJmpTrampoline(0x140130900, handleInput2, 0xE4);
 }
